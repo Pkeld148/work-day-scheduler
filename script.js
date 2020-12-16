8381582ca75eba48ce404c9990579d5740e1b871
Paul Keldsen <Pkeld148@gmail.com>
2020-12-16 16:46:19 -0500

    added indentifying attributes to elements, adjusted padding in css for neater look

--- script.js
+++ script.js
@@ -7,26 +7,41 @@ var dayHours = ["10", "11", "12", "13", "14", "15", "16", "17"];
 
 
 
-
-
-
-
-
-// Creating elements
-
 for (i=0; i < dayHours.length; i++) {
 var row = $("<div>");
 row.addClass("row time-block");
     var hour = $("<div>");
-    hour.addClass("hour col-1").text(dayHours[i]);
+    hour.addClass("hour col-1").text(dayHours[i] + ":00");
     row.append(hour);
 
     var textArea = $("<textarea>")
     textArea.addClass("past description col-10");
+    textArea.attr("textIndex", i+1);
     row.append(textArea);
 
     var button = $("<button>");
     button.addClass("saveBtn fas fa-save col-1");
+    button.attr("buttonIndex", i+1);
     row.append(button)
 $(".container").append(row);
 };
+
+$(".saveBtn").on("click", function() {
+    console.log("YOU CLICKED A SAVE BUTTON");
+    console.log("YOU CLICKED BUTTON WITH BUTTON INDEX = " + $(this).attr("buttonIndex"));
+
+});
+
+
+// var textIndex = $("textIndex");
+// textIndex.text("YO WAT UP");
+
+// $("<textarea>").textIndex[3].text("YO WAT UP");
+// $("#textIndex[3]").text("YO WAT UP");
+
+
+
+// TODO: GET CURRENT DATE/TIME TO DISPLAY AT TOP
+
+// var currentTime = $("#currentDay");
+// currentTime.text("FIGURE THIS MOMENT SHIT OUT LATER");
