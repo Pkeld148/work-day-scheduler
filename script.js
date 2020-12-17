038f4dcd9a8da3d31b4e90bbd5b105f165e5c5a5
Paul Keldsen <Pkeld148@gmail.com>
2020-12-16 20:16:23 -0500

    stuck trying to find way to store locally with indentifiers

--- script.js
+++ script.js
@@ -1,45 +1,40 @@
 var dayHours = ["10", "11", "12", "13", "14", "15", "16", "17"];
 
+var buttonEl = $(".saveBtn");
+var notesArea = $(".notes");
 
+var notesObject = {};
+var notesArray = [];
 
+// CREATING ROW ELEMENTS
+for (i = 0; i < dayHours.length; i++) {
+  var row = $("<div>");
+  row.addClass("row time-block");
+  var hour = $("<div>");
+  hour.addClass("hour col-1").text(dayHours[i] + ":00");
+  row.append(hour);
 
+  var textArea = $("<textarea>");
+  textArea.addClass("past description col-10");
+  textArea.attr("id", "notes" + (i + 1));
+  row.append(textArea);
 
+  var button = $("<button>");
+  button.addClass("saveBtn fas fa-save col-1");
+  button.attr("id", "button" + (i + 1));
+  row.append(button);
+  $(".container").append(row);
+}
 
-
-
-for (i=0; i < dayHours.length; i++) {
-var row = $("<div>");
-row.addClass("row time-block");
-    var hour = $("<div>");
-    hour.addClass("hour col-1").text(dayHours[i] + ":00");
-    row.append(hour);
-
-    var textArea = $("<textarea>")
-    textArea.addClass("past description col-10");
-    textArea.attr("textIndex", i+1);
-    row.append(textArea);
-
-    var button = $("<button>");
-    button.addClass("saveBtn fas fa-save col-1");
-    button.attr("buttonIndex", i+1);
-    row.append(button)
-$(".container").append(row);
-};
-
-$(".saveBtn").on("click", function() {
+$(document).ready(function () {
+  $(".saveBtn").on("click", function () {
     console.log("YOU CLICKED A SAVE BUTTON");
-    console.log("YOU CLICKED BUTTON WITH BUTTON INDEX = " + $(this).attr("buttonIndex"));
+    
+ $(".description").text("THIS FUCKING SUCKS");
 
+
+  });
 });
-
-
-// var textIndex = $("textIndex");
-// textIndex.text("YO WAT UP");
-
-// $("<textarea>").textIndex[3].text("YO WAT UP");
-// $("#textIndex[3]").text("YO WAT UP");
-
-
 
 // TODO: GET CURRENT DATE/TIME TO DISPLAY AT TOP
 
