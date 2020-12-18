070e4e584c36a809bf82a9646251eede0f208b66
Paul Keldsen <Pkeld148@gmail.com>
2020-12-17 19:12:29 -0500

    incorporated moment to change textarea color depending on time of day

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-var dayHours = ["10", "11", "12", "13", "14", "15", "16", "17"];
+var dayHours = ["10", "11", "12", "13", "14", "15", "16", "17", "18", "19"];
 
 var buttonEl = $(".saveBtn");
 var notesArea = $(".notes");
 var currentDay = $("#currentDay");
+var testDiv = $("#testDiv");
 
 
 // CREATING ROW ELEMENTS
@@ -10,31 +11,44 @@ for (i = 0; i < dayHours.length; i++) {
   var row = $("<div>");
   row.addClass("row time-block");
   var hour = $("<div>");
+  if (dayHours[i] > 12) {
+    hour.addClass("hour col-1").text(dayHours[i] - 12 + ":00");
+    row.append(hour);
+  } else {
   hour.addClass("hour col-1").text(dayHours[i] + ":00");
   row.append(hour);
+  }
+
+
 
   var textArea = $("<textarea>");
-  textArea.addClass("past description col-10");
-  textArea.attr("id", "notes" + (i + 1));
+  if (moment().format('HH') > dayHours[i]) {
+    textArea.addClass("past description col-10");
+  } else if (moment().format('HH') < dayHours[i]) {
+    textArea.addClass("future description col-10");
+  } else {
+    textArea.addClass("present description col-10");
+  }
+  textArea.attr("data-text", (i + 1));
   row.append(textArea);
 
   var button = $("<button>");
   button.addClass("saveBtn fas fa-save col-1");
-  button.attr("id", "button" + (i + 1));
+  button.attr("data-button", (i + 1));
   row.append(button);
   $(".container").append(row);
 }
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
-currentDay.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+currentDay.text(moment().format('MMMM Do YYYY'));
 
 $(document).ready(function () {
   $(".saveBtn").on("click", function () {
-    console.log("YOU CLICKED A SAVE BUTTON");
-
- $(".description").text("THIS FUCKING SUCKS");
-
-
+    var buttonIndex = $(this).attr("data-button");
+    console.log($(this).attr("data-button"));
+    console.log(" YOU CLICKED BUTTON: " + buttonIndex);
   });
+
+  
+
 });
 
 // TODO: GET CURRENT DATE/TIME TO DISPLAY AT TOP
