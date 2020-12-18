3fefab5d7713a409864cb632f7b29a1897db7398
Paul Keldsen <Pkeld148@gmail.com>
2020-12-17 22:22:12 -0500

    added screenshot, completed readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# work-day-scheduler
+# Work Day Scheduler
+
+## Description
+
+The goal of this project was to create a webpage that functions as personal planner.  The user can edit in notes for the different work hours and they will persist even through reloading the page.  This project utilizes javascript and the Moment API to provide a dynamic color display according to the user's current time.
+
+## Installation
+
+All functionality of this project contained within the deployed webpage itself.  Users can navigate to the following address to experience the application:
+https://pkeld148.github.io/work-day-scheduler/
+
+## Usage
+
+The user will be presented a series of text boxes which relate to the hour of the work day.  The user can edit in any messages or notes into these spaces.  Upon hitting any of the save buttons on the right side, the text in that corresponding space will be saved to local storage and will be visible in that space until manually deleted or edited over.
+
+![screenshot](screenshot-scheduler.png)
+
+## Credits
+
+The Georgia Tech Boot Camp provided the majority of the information needed to complete this project.
+
+## License
+
+Licensed under the [MIT](https://github.com/Pkeld148/work-day-scheduler/blob/main/LICENSE) license.
--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-var dayHours = ["10", "11", "12", "13", "14", "15", "16", "17", "18", "19"];
+var dayHours = ["10", "11", "12", "13", "14", "15", "16", "17"];
 
 var buttonEl = $(".saveBtn");
 var notesArea = $(".notes");
 var currentDay = $("#currentDay");
 var testDiv = $("#testDiv");
-
 
 // CREATING ROW ELEMENTS
 for (i = 0; i < dayHours.length; i++) {
@@ -15,43 +14,47 @@ for (i = 0; i < dayHours.length; i++) {
     hour.addClass("hour col-1").text(dayHours[i] - 12 + ":00");
     row.append(hour);
   } else {
-  hour.addClass("hour col-1").text(dayHours[i] + ":00");
-  row.append(hour);
+    hour.addClass("hour col-1").text(dayHours[i] + ":00");
+    row.append(hour);
   }
 
-
-
   var textArea = $("<textarea>");
-  if (moment().format('HH') > dayHours[i]) {
+  var getText = JSON.parse(localStorage.getItem(i + 1));
+
+  if (moment().format("HH") > dayHours[i]) {
     textArea.addClass("past description col-10");
-  } else if (moment().format('HH') < dayHours[i]) {
+  } else if (moment().format("HH") < dayHours[i]) {
     textArea.addClass("future description col-10");
   } else {
     textArea.addClass("present description col-10");
   }
-  textArea.attr("data-text", (i + 1));
+  textArea.attr("data-button", i + 1);
+  textArea.addClass("line" + (i + 1));
+  textArea.text(getText);
   row.append(textArea);
 
   var button = $("<button>");
   button.addClass("saveBtn fas fa-save col-1");
-  button.attr("data-button", (i + 1));
+  button.addClass("line" + (i + 1));
+  button.attr("id", i + 1);
+  button.attr("data-button", i + 1);
   row.append(button);
   $(".container").append(row);
 }
-currentDay.text(moment().format('MMMM Do YYYY'));
+currentDay.text(moment().format("MMMM Do YYYY"));
 
 $(document).ready(function () {
+  var firstTextArea = JSON.parse(localStorage.getItem("0"));
+  $("#firstText").text(firstTextArea);
+
   $(".saveBtn").on("click", function () {
     var buttonIndex = $(this).attr("data-button");
-    console.log($(this).attr("data-button"));
-    console.log(" YOU CLICKED BUTTON: " + buttonIndex);
+
+    var textNotes = [];
+    textNotes = $("textarea").val();
+    console.log(textNotes);
+    console.log(buttonIndex);
+
+    localStorage.setItem(buttonIndex, JSON.stringify(textNotes));
   });
-
-  
-
 });
-
-// TODO: GET CURRENT DATE/TIME TO DISPLAY AT TOP
-
-// var currentTime = $("#currentDay");
-// currentTime.text("FIGURE THIS MOMENT SHIT OUT LATER");
